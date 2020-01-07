b4767602541674c1d3a9b980c493c74b9caccb3e
Chester Hansen <chester@Chesters-MBP-2.hsd1.ut.comcast.net>
2020-01-06 20:41:17 -0700

    working on site infrastructure

---
+++ src/Pages/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+import {Row} from '../Components/Containers'
+
+
+
+export default function Main() {
+  return (
+    <div>
+      Art By Kanieshhh! GIVE ME YOUR MONIES!!!!
+    </div>
+  )
+}
