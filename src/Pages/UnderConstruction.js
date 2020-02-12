9b505dcbbe8bb6e2c47fda1d7690c44b7bb9a60e
Chester Hansen <chester.hansen@pillpack.com>
2020-02-12 07:40:55 -0700

    minor adjustments

--- src/Pages/UnderConstruction.js
+++ src/Pages/UnderConstruction.js
@@ -1,33 +1,38 @@
 import React from "react";
 
+import logoMain from "../Resourses/logo_main.png";
+
 import { MainCol, LogoRow } from "../Components/Styling/Containers";
 import { Title, Text } from "../Components/Styling/Content";
 import { FaceBook, Instagram } from "../Components/Logos";
 import { ThemeSub2 } from "../Components/Styling/Theme";
 
-export default function Portfolio() {
+export default function UnderConstruction() {
   return (
     <MainCol alignItems="center" justifyContent="space-around">
-      <Title title="logo goes here." size="3em">
-          Art By Kanieshhh!
-        </Title>
-        <br/>
+      <img
+        src={logoMain}
+        style={{ marginBottom: "15px" }}
+        alt=""
+      />
+
+      <br />
       <Title>Gone Hiking...</Title>
       <Text>This site is still being made. Please check back soon.</Text>
-      <LogoRow width={'25%'}>
+      <LogoRow width={"25%"}>
         <a
           href="https://www.facebook.com/artbykanieshhh/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FaceBook color={ThemeSub2}/>
+          <FaceBook color={ThemeSub2} />
         </a>
         <a
           href="https://www.instagram.com/artbykanieshhh/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Instagram color={ThemeSub2}/>
+          <Instagram color={ThemeSub2} />
         </a>
       </LogoRow>
     </MainCol>
